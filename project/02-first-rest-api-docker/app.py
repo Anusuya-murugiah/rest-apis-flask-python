c1abc7d626f1831794f249b089e5f5c3a98d2a26
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2026-03-30 15:50:56 +0530

    Update app.py

--- project/02-first-rest-api-docker/app.py
+++ project/02-first-rest-api-docker/app.py
@@ -21,7 +21,7 @@ def get_stores():
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
+#    request_data = request.get_json()
     new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
@@ -29,7 +29,7 @@ def create_store():
 
 @app.post("/store/<string:name>/item")
 def create_item(name):
-    request_data = request.get_json()
+#    request_data = request.get_json()
     for store in stores:
         if store["name"] == name:
             new_item = {"name": request_data["name"], "price": request_data["price"]}
